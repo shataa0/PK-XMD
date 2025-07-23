d7fde68b20ee4c5ffa8ad3945651b160290db2c1
mejjar00254 <pkdrillermxd@gmail.com>
2025-07-23 21:44:22 +0300

    Update main-ban.js

--- plugins/main-ban.js
+++ plugins/main-ban.js
@@ -66,7 +66,7 @@ cmd({
         fs.writeFileSync("./assets/ban.json", JSON.stringify(updated, null, 2));
 
         await conn.sendMessage(from, {
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/4o9kvi.jpg" },
             caption: `✅ User has been unbanned.`
         }, { quoted: mek });
 
@@ -98,7 +98,7 @@ cmd({
         });
 
         await conn.sendMessage(from, {
-            image: { url: "https://files.catbox.moe/7zfdcq.jpg" },
+            image: { url: "https://files.catbox.moe/4o9kvi.jpg" },
             caption: msg
         }, { quoted: mek });
     } catch (err) {
