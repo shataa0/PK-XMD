4b787c2e826ad1c95c307ee61c568b715792bb0e
mejjar00254 <pkdrillermxd@gmail.com>
2025-07-23 22:50:05 +0300

    Update Quran.js

--- plugins/Quran.js
+++ plugins/Quran.js
@@ -451,8 +451,8 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363288304618280@newsletter',
+                        newsletterName: 'pkdriller',
                         serverMessageId: 143
                     }
                 }
