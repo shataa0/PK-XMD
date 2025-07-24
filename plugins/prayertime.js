7d76ef648b1b873475911e6f46deadd030784671
mejjar00254 <pkdrillermxd@gmail.com>
2025-07-24 17:51:15 +0300

    Update prayertime.js

--- plugins/prayertime.js
+++ plugins/prayertime.js
@@ -53,15 +53,15 @@ async(conn, mek, m, {from, l, quoted, body, isCmd, command, args, q, isGroup, se
         await conn.sendMessage(
             from,
             {
-                image: { url: `https://files.catbox.moe/8fy6up.jpg` },
+                image: { url: `https://files.catbox.moe/lvrpek.jpg` },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
                     forwardingScore: 999,
                     isForwarded: true,
                     forwardedNewsletterMessageInfo: {
-                        newsletterJid: '120363354023106228@newsletter',
-                        newsletterName: 'JawadTechX',
+                        newsletterJid: '120363288304618280@newsletter',
+                        newsletterName: 'pkxmd',
                         serverMessageId: 143
                     }
                 }
